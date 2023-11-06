3ca57d0d72b7565430e336a75634cb0b3cc3d383
Andrew Desmarais <andydes@amazon.com>
2023-11-06 14:12:06 -0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/andy-base-path',
+  // assetPrefix: 'http://andy.test/asset-prefix',
+  // assetPrefix: '/some-asset-prefix',
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'images.unsplash.com', 
+      'source.unsplash.com', 
+      'skylark-doc.s3.ca-central-1.amazonaws.com',
+      'main.dnwbx4xe2r772.amplifyapp.com'
+    ],
+  },
+};
+
+module.exports = nextConfig
